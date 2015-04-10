66f79e7abcc22b7ef2d1b29113e1c0bf79490464
Ricardo Rivas G <richistron@gmail.com>
2015-04-10 17:34:53 -0500

    tests for products service

--- app/scripts/services/products.js
+++ app/scripts/services/products.js
@@ -11,33 +11,33 @@ angular.module('carritoApp').factory('products', function () {
   var products = [
     {
       id:           1,
-      name:         "MacBook Pro 15 inches",
+      name:         'MacBook Pro 15 inches',
       price:        21500.30,
-      description:  "Retina display, 8 GB RAM.",
+      description:  'Retina display, 8 GB RAM.',
       stock:        15
     }, {
       id:           2,
-      name:         "MacBook Pro 15 inches",
+      name:         'MacBook Pro 15 inches',
       price:        28500.30,
-      description:  "Retina display, 16 GB RAM.",
+      description:  'Retina display, 16 GB RAM.',
       stock:        15
     }, {
       id:           3,
-      name:         "MacBook Air 13 inches",
+      name:         'MacBook Air 13 inches',
       price:        1500.20,
-      description:  "Retina display, 16 GB RAM.",
+      description:  'Retina display, 16 GB RAM.',
       stock:        15
     }, {
       id:           4,
-      name:         "MacBook Air 13 inches",
+      name:         'MacBook Air 13 inches',
       price:        1000.50,
-      description:  "Retina display, 8 GB RAM.",
+      description:  'Retina display, 8 GB RAM.',
       stock:        15
     }, {
       id:           5,
-      name:         "Mouse path",
+      name:         'Mouse path',
       price:        100,
-      description:  "Magic apple path",
+      description:  'Magic apple path',
       stock:        300
     }
   ];
--- test/spec/services/products.js
+++ test/spec/services/products.js
@@ -15,4 +15,57 @@ describe('Service: products', function () {
     expect(!!products).toBe(true);
   });
 
+  it('should have a get method', function () {
+    expect(products.get).toBeDefined();
+  });
+
+  it('should return a non empty array', function () {
+    expect(products.get().length).toBeGreaterThan(1);
+  });
+
+
+  it('each element should have a description', function () {
+    products.get().forEach(function (product) {
+      expect(product.description).toBeDefined();
+      expect(product.description.length).toBeGreaterThan(15);
+    });
+  });
+
+
+  it('each element should have a price', function () {
+    products.get().forEach(function (product) {
+      expect(product.price).toBeDefined();
+      expect(product.price).toEqual(jasmine.any(Number));
+      expect(product.price).toBeGreaterThan(1);
+    });
+  });
+
+  it('each element should have a name', function () {
+    products.get().forEach(function (product) {
+      expect(product.name).toBeDefined();
+      expect(product.name.length).toBeGreaterThan(5);
+    });
+  });
+
+  it('each element should have a id', function () {
+    products.get().forEach(function (product) {
+      expect(product.id).toBeDefined();
+      expect(product.price).toEqual(jasmine.any(Number));
+    });
+  });
+
+  it('each id should be unique', function () {
+    var ids = [];
+    products.get().forEach(function (product) {
+      expect(ids).not.toContain(product.id);
+      ids.push(product.id);
+    });
+  });
+
+  it('each element should have a stock', function () {
+    products.get().forEach(function (product) {
+      expect(product.stock).toBeDefined();
+      expect(product.stock).toBeGreaterThan(0);
+    });
+  });
 });
